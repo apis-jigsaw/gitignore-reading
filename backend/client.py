be362a06cd2f9213f0cf841b831fada30f7a1abf
Jeff Katzy <jeffreyerickatz@gmail.com>
2022-09-19 16:52:02 -0400

    update

--- backend/client.py
+++ backend/client.py
@@ -1,10 +1,9 @@
 import requests
 from settings import CLIENT_ID, CLIENT_SECRET, DATE
-
 class Client:
-    CLIENT_ID = CLIENT_ID
-    CLIENT_SECRET = CLIENT_SECRET
-    DATE = DATE
+    CLIENT_ID=CLIENT_ID
+    CLIENT_SECRET=CLIENT_SECRET
+    DATE=DATE
     URL = "https://api.foursquare.com/v2/venues/search"
     SHOW_URL = "https://api.foursquare.com/v2/venues"
 
@@ -21,8 +20,3 @@ class Client:
     def request_venues(self, query_params = {'ll': "40.7,-74", "query": "tacos"}):
         response = requests.get(self.URL, self.full_params(query_params))
         return response.json()['response']['venues']
-
-    def request_venue(self, venue_id):
-        response = requests.get(f"{self.SHOW_URL}/{venue_id}", self.auth_params())
-        return response.json()['response']['venue']
-
--- backend/run.py
+++
@@ -1,5 +0,0 @@
-from settings import DB_NAME, DB_USER
-from api import create_app
-app = create_app(DB_NAME, DB_USER)
-
-app.run(debug = True)
